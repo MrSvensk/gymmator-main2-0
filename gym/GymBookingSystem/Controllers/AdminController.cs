eac87a57fb81e5cad44bc26454cdf3626a6b1938
Modersohn16 <40393958+Modersohn16@users.noreply.github.com>
2020-12-26 20:52:33 +0100

    AdminController & Service update

--- gym/GymBookingSystem/Controllers/AdminController.cs
+++ gym/GymBookingSystem/Controllers/AdminController.cs
@@ -6,11 +6,100 @@ using System.Threading.Tasks;
 
 namespace GymBookingSystem.Controllers
 {
-    public class AdminController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
     {
-        public IActionResult Index()
+        private IAdminService _AdminService;
+        public AdminController(IAdminService AdminService)
         {
-            return View();
+            _AdminService = AdminService;
+        }
+
+        [HttpPost("CreateUser")]
+        public IActionResult CreateUser(UserDto dto)
+        {
+            User u = _AdminService.CreateUser(dto);
+            if (u == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Created("yay", u);
+            }
+        }
+
+        [HttpPost("CreateGym")]
+        public IActionResult CreateGym(GymDto dto)
+        {
+            Gym g = _AdminService.CreateGym(dto);
+            if (g == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Created("yay", g);
+            }
+        }
+
+        [HttpPost("CreateTrainingClass")]
+        public IActionResult CreateTrainingClass(TrainingClassDto dto)
+        {
+            TrainingClass t = _AdminService.CreateTrainingClass(dto);
+            if (t == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Created("yay", t);
+            }
+        }
+
+
+        [HttpGet("GetUsersBookings")]
+        public IActionResult GetUsersBookings(int userId)
+        {
+            List<Booking> b = _AdminService.GetUsersBookings(userId);
+            if (b == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(b);
+            }
+        }
+
+        [HttpPost("CreateTrainer")]
+        public IActionResult CreateTrainer(TrainerDto dto)
+        {
+            Trainer t = _AdminService.CreateTrainer(dto);
+            if (t == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Created("Tjoho", t);
+            }
+        }
+
+
+        [HttpDelete("DeleteUser")]
+        public IActionResult DeleteUser(int UserId)
+        {
+            User U = _AdminService.DeleteUser(UserId);
+            if (U == null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return Ok(U);
+            }
         }
     }
 }
--- gym/GymBookingSystem/Services/IAdminService.cs
+++ gym/GymBookingSystem/Services/IAdminService.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GymBookingSystem.Models;
+using GymBookingSystem.Models.DTO;
 
 namespace GymBookingSystem.Services
 {
-    public class IAdminService
+    public interface IAdminService
     {
+        User CreateUser(UserDto dto);
+        Gym CreateGym(GymDto dto);
+
+        TrainingClass CreateTrainingClass(TrainingClassDto dto);
+        User DeleteUser(int UserId);
+        List<Booking> GetUsersBookings(int userId);
+        Trainer CreateTrainer(TrainerDto dto);
+
     }
 }
